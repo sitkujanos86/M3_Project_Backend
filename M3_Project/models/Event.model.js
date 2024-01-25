0d595348ec5ab83d3da455a478ffe0c4292784e9
Prabeshika <prabeshika14@gmail.com>
2024-01-25 22:30:31 +0100

    handled error

--- M3_Project/models/Event.model.js
+++ M3_Project/models/Event.model.js
@@ -1,47 +1,46 @@
-const { Schema, model, Types } = require('mongoose')
+const { Schema, model, Types } = require("mongoose");
 
-// TODO: Please make sure you edit the Book model to whatever makes sense in this case
 const eventSchema = new Schema(
   {
     title: {
       type: String,
-      required: [true, 'Title is required.'],
+      required: [true, "Title is required."],
       trim: true,
     },
     organiser: {
       type: String,
-      required: [true, 'Author is required.'],
+      required: [true, "Author is required."],
       trim: true,
     },
     date: {
       type: Date,
-      required: [true, 'Pages is required.'],
+      required: [true, "Pages is required."],
     },
     location: {
       type: String,
-      required: [true, 'Pages is required.'],
+      required: [true, "Pages is required."],
     },
     price: {
       type: Number,
     },
     description: {
       type: String,
-      required: [true, 'Pages is required.'],
+      required: [true, "Pages is required."],
     },
     image: {
       type: String,
     },
     createdBy: {
       type: Types.ObjectId,
-      ref: 'User',
+      ref: "User",
     },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
-)
+);
 
-const Book = model('Event', eventSchema)
+const Event = model("Event", eventSchema);
 
-module.exports = Book
+module.exports = Event;
--- M3_Project/routes/auth.routes.js
+++ M3_Project/routes/auth.routes.js
@@ -1,79 +1,81 @@
-const User = require('../models/User.model');
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const router = require('express').Router()
+const User = require("../models/User.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = require("express").Router();
 
-const { isAuthenticated } = require('../middlewares/route-guard.middleware')
-router.get('/', (req, res) => {
-    res.json('All good in auth')
-})
+const { isAuthenticated } = require("../middlewares/route-guard.middleware");
+router.get("/", (req, res) => {
+  res.json("All good in auth");
+});
 
 // SIGNUP
-router.post('/signup', async (req, res) => {
-    const { email, password } = req.body
-    console.log(req.body);
-    try {
-        const potentialUser = await User.findOne({ email })
-        if (!potentialUser) {
-            const salt = bcrypt.genSaltSync(13)
-            const hashedPassword = bcrypt.hashSync(password, salt)
-            try {
-                await User.create({ email, hashedPassword })
-                res.status(201).json({ message: 'User created' })
-            } catch (error) {
-                res.status(500).json({ message: "Problem with creating user" })
-            }
-
-        }
-        else {
-            res.status(400).json({ message: 'The email is already in use' })
-
-        }
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  try {
+    const potentialUser = await User.findOne({ email });
+    if (!potentialUser) {
+      const salt = bcrypt.genSaltSync(13);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      try {
+        await User.create({ email, hashedPassword });
+        res.status(201).json({ message: "User created" });
+      } catch (error) {
+        res.status(500).json({ message: "Problem with creating user" });
+      }
+    } else {
+      res.status(400).json({ message: "The email is already in use" });
     }
-    catch (error) {
-        console.log(error)
-        res.status(500).json({ message: 'There was an error while trying to get a user' })
-    }
-})
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "There was an error while trying to get a user" });
+  }
+});
 
 //LOGIN
-// ASK TA WHY ID IS WITH "_id" 
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body
-    try {
-        const potentialUser = await User.findOne({ email })
+// ASK TA WHY ID IS WITH "_id"
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const potentialUser = await User.findOne({ email });
 
-        if (potentialUser) {
-            const passwordCorrect = bcrypt.compareSync(password, potentialUser.hashedPassword)
-            if (passwordCorrect) {
-                const authToken = jwt.sign({ userId: potentialUser._id }, process.env.TOKEN_SECRET, {
-                    algorithm: 'HS256',
-                    expiresIn: '6h',
-                })
-                res.status(200).json({ token: authToken })
-            }
-            else {
-                // Incorrect password
-                console.log('Problem with password');
-                res.status(403).json({ message: "Something went wrong"})
-            }
-        }
-        else {
-            // No user with this email
-            console.log('Problem while checking for the email')
-            res.status(403).json({ message: 'Something went wrong' })
-        }
+    if (potentialUser) {
+      const passwordCorrect = bcrypt.compareSync(
+        password,
+        potentialUser.hashedPassword
+      );
+      if (passwordCorrect) {
+        const authToken = jwt.sign(
+          { userId: potentialUser._id },
+          process.env.TOKEN_SECRET,
+          {
+            algorithm: "HS256",
+            expiresIn: "6h",
+          }
+        );
+        res.status(200).json({ token: authToken });
+      } else {
+        // Incorrect password
+        console.log("Problem with password");
+        res.status(403).json({ message: "Something went wrong" });
+      }
+    } else {
+      // No user with this email
+      console.log("Problem while checking for the email");
+      res.status(403).json({ message: "Something went wrong" });
     }
-    catch (error) {
-        console.log(error)
-        res.status(500).json({ message: 'Server Error' })
-    }
-})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 
-router.get('/verify', isAuthenticated, async (req, res) => {
-    console.log(req.tokenPayload)
-    const currentUser = await User.findById(req.tokenPayload.userId)
-    res.status(200).json(currentUser)
-  })
-  
-  module.exports = router
+router.get("/verify", isAuthenticated, async (req, res) => {
+  console.log(req.tokenPayload);
+  const currentUser = await User.findById(req.tokenPayload.userId);
+  res.status(200).json(currentUser);
+});
+
+module.exports = router;
